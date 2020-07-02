289933a48115ba3f1a760bcf00c1e444377bcb00
Ayooluwa-05 <zoladiran@gmail.com>
2020-07-02 12:39:51 +0100

    demo js changes

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,7 +1,7 @@
 var events = [
-  {'Date': new Date(2020, 7, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-  {'Date': new Date(2020, 7, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-  {'Date': new Date(2020, 7, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+  {'Date': new Date(2020, 6, 7), 'Title': 'Destiny College.'},
+  {'Date': new Date(2020, 6, 18), 'Title': 'Reality Conference!'},
+  {'Date': new Date(2020, 6, 27), 'Title': 'Prayer Conference'},
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
